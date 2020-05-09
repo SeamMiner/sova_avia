a99f3e159ba0db75a1b037cef4c271b77964d61c
DaniilGorokhov <kripa0cool@mail.ru>
2020-05-09 03:41:55 +0300

    fix bag with login

--- apps/index/views.py
+++ apps/index/views.py
@@ -17,14 +17,14 @@ def register(request):
 def auth(request):
     try:
         user_instance = User.objects.get(login=request.POST['login'])
-        if user_instance.password == request.POST['login']:
-            return HttpResponseRedirect(reverse('index/register.html'))  # Следует переписать путь до страницы с курсами
+        if user_instance.password.strip("'") == request.POST['password']:
+            return HttpResponseRedirect(reverse('index:register'))  # Следует переписать путь до страницы с курсами
         else:
-            return render(request, 'index/incorrect.html')
+            return render(request, 'index/incorrect.html', {'login': request.POST['login']})
     except:
         try:
             admin_instance = Admin.objects.get(login=request.POST['login'])
-            if admin_instance.password == request.POST['login']:
-                HttpResponseRedirect(reverse('index/register.html'))  # Следует переписать путь до страницы админ панели
+            if admin_instance.password == request.POST['password']:
+                HttpResponseRedirect(reverse('index:register'))  # Следует переписать путь до страницы админ панели
         except:
-            return render(request, 'index/incorrect.html')
+            return render(request, 'index/incorrect.html', {'login': request.POST['login']})
