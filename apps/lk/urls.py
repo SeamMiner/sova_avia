db559f8db35f391dcfd3634be56f8df312280f20
DaniilGorokhov <kripa0cool@mail.ru>
2020-05-10 13:21:05 +0300

    add viewed courses by user

--- apps/lk/urls.py
+++ apps/lk/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 app_name = 'lk'
 urlpatterns = [
-    path('<str:user_login>', views.index, name="index")
+    path('<str:user_login>', views.index, name="index"),
+    path('<str:user_login>/view_courses', views.view_courses, name="view_courses"),
 ]
--- apps/lk/views.py
+++ apps/lk/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import Http404
 from apps.index.models import User, Admin
-from apps.admin_panel.models import Specialization, StaticGraph
+from apps.admin_panel.models import Specialization, StaticGraph, Course
 
 
 def index(request, user_login):
@@ -10,6 +10,14 @@ def index(request, user_login):
     except:
         raise Http404
     return render(request, 'lk/index.html', {'user_instance': user, 'list_companies': Admin.objects.all(),
-                                             'list_spec': Specialization.objects.all(),
+                                             'list_spec': Specialization.objects.all(), 'user_login': user_login,
                                              'ex_image0': StaticGraph.objects.all()[0],
                                              'ex_image1': StaticGraph.objects.all()[1]})
+
+
+def view_courses(request, user_login):
+    try:
+        user = User.objects.get(login=user_login)
+    except:
+        raise Http404
+    return render(request, 'lk/courses_list.html', {'courses_list': Course.objects.all()})
